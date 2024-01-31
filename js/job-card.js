a76bc6430428c5f190292dcec48bced3f86df27d
Uday1004 <Udaysolanki530@gmail.com>
2024-01-31 18:11:10 +0530

    job apply

--- js/job-card.js
+++ js/job-card.js
@@ -1,26 +1,67 @@
 // Sample JSON data
 const jobData = [
-  { "title": "Software Support Engineer", "company": "Information Technology", "businessArea":"Technology",  "location": "hydrabad | India" , "url":"Career/job-title.html" },
-  { "title": "Software Engineer - Java", "company": "Software Development", "businessArea":"Technology",  "location": "Indore | India" , "url":'Career/job-title.html' },
-  { "title": "Senior Software Engineer", "company": "Software Engineering", "businessArea":"Technology",  "location": "Chicago | USA","url":'Career/job-title.html' },
-  { "title": "Senior Quality Analyst", "company": "Software Testing & QA", "businessArea":"Technology",  "location": "Indore | India","url":'Career/job-title.html' },
-  { "title": "Software Engineer - Nodejs/Angular", "company": "Full-Stack Web Development", "businessArea":"Technology",  "location": "Indore | India","url":'Career/job-title.html' },
-  { "title": "Dev Ops Engineer - Google Cloud", "company": "DevOps Engineer", "businessArea":"Technology",  "location": "Indore | India","url":'Career/job-title.html' },
-  { "title": "Data Engineer", "company": "Data Engineering", "businessArea":"Technology",  "location": "Indore | India","url":'Career/job-title.html' }
+  {
+    title: "Software Support Engineer",
+    company: "Information Technology",
+    businessArea: "Technology",
+    location: "hydrabad | India",
+    url: "Career/job-title.html",
+  },
+  {
+    title: "Software Engineer - Java",
+    company: "Software Development",
+    businessArea: "Technology",
+    location: "Indore | India",
+    url: "Career/job-title.html",
+  },
+  {
+    title: "Senior Software Engineer",
+    company: "Software Engineering",
+    businessArea: "Technology",
+    location: "Chicago | USA",
+    url: "Career/job-title.html",
+  },
+  {
+    title: "Senior Quality Analyst",
+    company: "Software Testing & QA",
+    businessArea: "Technology",
+    location: "Indore | India",
+    url: "Career/job-title.html",
+  },
+  {
+    title: "Software Engineer - Nodejs/Angular",
+    company: "Full-Stack Web Development",
+    businessArea: "Technology",
+    location: "Indore | India",
+    url: "Career/job-title.html",
+  },
+  {
+    title: "Dev Ops Engineer - Google Cloud",
+    company: "DevOps Engineer",
+    businessArea: "Technology",
+    location: "Indore | India",
+    url: "Career/job-title.html",
+  },
+  {
+    title: "Data Engineer",
+    company: "Data Engineering",
+    businessArea: "Technology",
+    location: "Indore | India",
+    url: "Career/job-title.html",
+  },
 ];
 
-  // Get the container element
-  const jobCardContainer = $("#jobCardContainer");
+// Get the container element
+const jobCardContainer = $("#jobCardContainer");
 
-  // Map through the JSON data and create Owl Carousel items
-  jobData.map(job => {
-    // Create a div element for each job
-    const jobCard = document.createElement('div');
-    jobCard.classList.add('job-card');
-    
+// Map through the JSON data and create Owl Carousel items
+jobData.map((job) => {
+  // Create a div element for each job
+  const jobCard = document.createElement("div");
+  jobCard.classList.add("job-card");
 
-    // Populate the div with job data
-    jobCard.innerHTML = `
+  // Populate the div with job data
+  jobCard.innerHTML = `
       <div class="job-details">
         <h2><a href="${job.url}" target="_blank" class='job-title'>${job.title}</a></h2>
         <p class="company">${job.company}</p>
@@ -34,41 +75,38 @@ const jobData = [
       </div>
     `;
 
-    // Append the job card to the container
-    jobCardContainer.append(jobCard);
-  });
+  // Append the job card to the container
+  jobCardContainer.append(jobCard);
+});
 
-  // Initialize Owl Carousel with custom navigation and hover stop
-  const owl = jobCardContainer.owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: false, // Disable default navigation
-    dots: false, // Disable default dots
-    
-    responsive: {
-      0: {
-        items: 1
-      },
-      600: {
-        items: 2
-      },
-      1000: {
-        items: 3
-      }
+// Initialize Owl Carousel with custom navigation and hover stop
+const owl = jobCardContainer.owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: false, // Disable default navigation
+  dots: false, // Disable default dots
+
+  responsive: {
+    0: {
+      items: 1,
     },
-    autoplay: true, // Enable autoplay
-    autoplayTimeout: 3000, // Set autoplay interval in milliseconds (e.g., 3000 ms = 3 seconds)
-    autoplayHoverPause: true, // Pause autoplay on hover
-     
-  });
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+  autoplay: true, // Enable autoplay
+  autoplayTimeout: 3000, // Set autoplay interval in milliseconds (e.g., 3000 ms = 3 seconds)
+  autoplayHoverPause: true, // Pause autoplay on hover
+});
 
-  // Custom navigation button events
-  $(".custom-prev-btn").on("click", function () {
-    owl.trigger("prev.owl.carousel");
-  });
+// Custom navigation button events
+$(".custom-prev-btn").on("click", function () {
+  owl.trigger("prev.owl.carousel");
+});
 
-  $(".custom-next-btn").on("click", function () {
-    owl.trigger("next.owl.carousel");
-  });
- 
-  
+$(".custom-next-btn").on("click", function () {
+  owl.trigger("next.owl.carousel");
+});
