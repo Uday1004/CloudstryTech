5bee0e283c7cf1a21a40c5aa44df3cc269607e16
Uday1004 <Udaysolanki530@gmail.com>
2024-02-18 09:24:32 +0530

    gi

--- js/path.js
+++
@@ -1,9 +0,0 @@
-
-// Remove .html extension from the URL
- // Check if the URL ends with .html
- if (window.location.href.endsWith('.html')) {
-    // Remove the last 5 characters (.html)
-    var newLocation = window.location.href.slice(0, -5);
-    // Redirect to the new URL
-    window.location.href = newLocation;
-}
